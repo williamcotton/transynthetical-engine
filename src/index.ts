aff5218e62b6fdc37aecd69c0fef30d55e54a216
William Cotton <williamcotton@gmail.com>
2023-02-22 09:12:19 -0600

    add Makefile

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { ask } from "./ask";
 import { dispatch } from "./dispatch";
+import { analyticAugmentations } from "./analytic-augmentations";
 
 const prompt =
   "What is the population of Geneseo, NY combined with the population of Rochester, NY, divided by string length of the answer to the question 'What is the capital of France?'?";
@@ -7,4 +8,5 @@ const prompt =
 ask({
   prompt,
   dispatch,
+  analyticAugmentation: analyticAugmentations[2],
 }).then((solution) => console.log(solution));
